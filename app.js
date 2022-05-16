54477d4bbdbca6bc563e14d6e6ce8b8acefa58c8
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-05-16 02:40:30 +0200

    Update files

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const btnStart = document.querySelector('.buttons button:nth-child(1)');
-const btnReset = document.querySelector('.buttons button:nth-child(2)');
-const cards = document.querySelectorAll('.card');
+const btnStart = document.querySelector('#btnStart');
+const btnReset = document.querySelector('#btnReset');
+const cards = document.querySelectorAll('img');
 
 console.log(cards);
 
 btnStart.addEventListener('click', () => {
   for (const card of cards) {
-    card.classList.toggle('rotated');
+    card.classList.add('rotated');
   }
 
   // cards[0].classList.toggle('sort');
   // cards[1].classList.toggle('sort');
   // cards[2].classList.toggle('sort');
 
-  btnStart.style.display = 'none';
-  btnReset.style.display = 'none';
+  // btnStart.style.display = 'none';
+  // btnReset.style.display = 'none';
 });
 
 btnReset.addEventListener('click', () => {
@@ -23,11 +23,11 @@ btnReset.addEventListener('click', () => {
   }
 });
 
-for (const card of cards) {
-  card.addEventListener('click', () => {
-    card.classList.remove('rotated');
-    card.classList.remove('reverse-sort');
-    card.classList.remove('sort');
-    card.classList.add('reverse-rotated');
-  });
-}
+// for (const card of cards) {
+//   card.addEventListener('click', () => {
+//     card.classList.remove('rotated');
+//     card.classList.remove('reverse-sort');
+//     card.classList.remove('sort');
+//     card.classList.add('reverse-rotated');
+//   });
+// }
